52bbc0d9c2bfdaeb5492312e7b20b2b041e74452
vSergeev <sergeevviktor017@gmail.com>
2018-07-21 22:04:27 +0300

    java config for spring mvc

---
+++ src/main/java/com/graphql/tutorial/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.graphql.tutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.graphql.tutorial"})
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+}
